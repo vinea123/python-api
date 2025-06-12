e68c230cf61de1836816bc42fee6437c9a6d52e7
seng vinea <sengvinea297@gmail.com>
2025-06-12 15:02:06 +0700

    pythhon api

--- routes/api.py
+++ routes/api.py
@@ -2,19 +2,35 @@ from http.server import BaseHTTPRequestHandler
 from controller.welcome import welcome_handler
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    ROUTES = {
-        "/": welcome_handler,
-    }
+    # welcome.py
+    ROUTES_GET = { "/": welcome_handler }
+    ROUTES_POST = {"/": welcome_handler}
+    ROUTES_PUT = {"/": welcome_handler}
+    ROUTES_DELETE = {"/": welcome_handler}
 
-    def do_GET(self):
-        handler = self.ROUTES.get(self.path)
+    def handle_request(self, method_routes):
+        handler = method_routes.get(self.path)
         if handler:
             status, headers, body = handler()
         else:
-            status, headers, body = 404, {"Content-Type": "application/json"}, b'{"error": "Not Found"}'
+            status = 404
+            headers = {"Content-Type": "application/json"}
+            body = b'{"error": "Not Found"}'
 
         self.send_response(status)
         for key, value in headers.items():
             self.send_header(key, value)
         self.end_headers()
         self.wfile.write(body)
+
+    def do_GET(self):
+        self.handle_request(self.ROUTES_GET)
+
+    def do_POST(self):
+        self.handle_request(self.ROUTES_POST)
+
+    def do_PUT(self):
+        self.handle_request(self.ROUTES_PUT)
+
+    def do_DELETE(self):
+        self.handle_request(self.ROUTES_DELETE)
